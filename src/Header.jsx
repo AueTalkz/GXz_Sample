b46d125af1e6fd938ededda0730179bb4b8caf44
Vasudev Kumar <162024257+AueTalkz@users.noreply.github.com>
2026-01-15 10:37:52 +0530

    Update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -13,7 +13,7 @@ function Header() {
                 <li><a href="index.html">Home</a></li>
                 <li><a href="#services">Services</a></li>
                 <li><a href="#team">Team</a></li>
-                <li><a href="#works">Works</a></li>
+                <li><a href="#projects">Projects</a></li>
                 <li><a href="#contact">Contact</a></li>
                </ul>
 
